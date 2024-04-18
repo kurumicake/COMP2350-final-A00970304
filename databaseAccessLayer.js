d9563f5871c26957891baafcd0863bc7621f3c7d
Christy Wan <christy.wan.965@gmail.com>
2024-04-18 14:52:23 -0700

    adding data to database

--- databaseAccessLayer.js
+++ databaseAccessLayer.js
@@ -1,22 +1,19 @@
-const database = include('/databaseConnection');
-
+const database = require('./databaseConnection');
 
 async function getAllItems() {
-	let sqlQuery = `
-		SELECT purchase_item_id, item_name, item_description, cost, quantity FROM purchase_item
-	`;
-	
-	try {
-		const results = await database.query(sqlQuery);
-		console.log(results[0]);
-		return results[0];
-	}
-	catch (err) {
-		console.log("Error selecting from Purchase Item table");
-		console.log(err);
-		return null;
-	}
+    let sqlQuery = `
+        SELECT purchase_item_id, item_name, item_description, cost, quantity FROM purchase_item
+    `;
+    
+    try {
+        const results = await database.query(sqlQuery);
+        console.log(results);
+        return results;
+    }
+    catch (err) {
+        console.error("Error selecting from Purchase Item table", err);
+        throw err;
+    }
 }
 
-
-module.exports = {getAllItems}
+module.exports = { getAllItems };
